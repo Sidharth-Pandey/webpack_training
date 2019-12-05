50cd5634196d9933218ce700fef21c911e2fa625
Siddharth Pandey <siddharth.pandey@nagra.com>
2019-12-05 16:18:37 +0530

    image handling
    Added html-loader to automatically require the files we reference in img tags
    Added file-loader to handle the svg,png,jpg,gif files from our images
    Configured file-loader to add our images to an imgs directory in dist
    Also configured it to add a hash to their filenames
    Lastly, added clean-webpack-plugin to our production config to clean out the dist directory each time we build

--- webpack-prod-config.js
+++ webpack-prod-config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const merge = require('webpack-merge');
 const  common = require('./webpack.common.js');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 module.exports = merge(common, {
   mode : "production",
   /*devtool : "none",*/
@@ -11,6 +11,9 @@ module.exports = merge(common, {
     /*filename: "main.js",*/
     filename: "main.[contentHash].js",
     path : path.resolve(__dirname, "dist")
-  }
+  },
+  plugins: [
+    new CleanWebpackPlugin()
+  ]
 
 });
